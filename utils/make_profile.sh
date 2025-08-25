abb46db4708800d5ff2514a2f329abe08c01ee67
Tom Wiesing <tkw01536@gmail.com>
2025-08-25 09:29:57 +0200

    Allow building profile for current year

--- utils/make_profile.sh
+++ utils/make_profile.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
+set -e
 
-YEAR=$1
-if [ -z "$YEAR" ]; then
+HISTORIC_YEAR=$1
+if [ -z "$HISTORIC_YEAR" ]; then
     echo "Need Year" >&2
     exit 1
 fi;
+
+# temporary tag to use for generating the profile
 TAGNAME="historic-texlive-profile"
 
-docker build --no-cache --build-arg HISTORIC_MIRROR=https://pi.kwarc.info/historic/ --build-arg HISTORIC_YEAR=${YEAR} -t "$TAGNAME" - 1>&2 <<"END"
+# Source for the texlive iso images.
+# We use our own mirrors here, as that doesn't waste anyone else's bandwidth. 
+
+# The default "historic" image source.
+HISTORIC_ISO_URL="https://pi.kwarc.info/historic/systems/texlive/${HISTORIC_YEAR}/texlive${HISTORIC_YEAR}.iso"
+# The 'current' image source.
+CURRENT_ISO_URL="https://ftp.fau.de/ctan/systems/texlive/Images/texlive${HISTORIC_YEAR}.iso"
+
+# Use the historic image url by default.
+# Change this to build a current profile.
+ISO_URL="${HISTORIC_ISO_URL}"
+
+docker build --no-cache --build-arg "ISO_URL=${ISO_URL}" --build-arg "HISTORIC_YEAR=${HISTORIC_YEAR}" -t "$TAGNAME" - 1>&2 <<"END"
 FROM debian:trixie AS base
 
 FROM base AS setup
@@ -21,9 +36,9 @@ RUN apt-get update \
         wget
 
 # Download the installer, and put all the files into /installer/
-ARG HISTORIC_MIRROR
+ARG ISO_URL
 ARG HISTORIC_YEAR
-RUN wget ${HISTORIC_MIRROR}/systems/texlive/${HISTORIC_YEAR}/texlive${HISTORIC_YEAR}.iso \
+RUN wget "$ISO_URL" \
     && mkdir /installer/ \
     && bsdtar xvpC /installer/ -f texlive${HISTORIC_YEAR}.iso \
     && rm texlive${HISTORIC_YEAR}.iso
@@ -32,5 +47,5 @@ RUN wget ${HISTORIC_MIRROR}/systems/texlive/${HISTORIC_YEAR}/texlive${HISTORIC_Y
 RUN cd /installer/; echo i | perl ./install-tl -repository /installer/
 END
 
-docker run -ti --rm "$TAGNAME" cat /usr/local/texlive/${YEAR}/tlpkg/texlive.profile > "texlive-${YEAR}.profile"
+docker run -ti --rm "$TAGNAME" cat /usr/local/texlive/${HISTORIC_YEAR}/tlpkg/texlive.profile > "texlive-${HISTORIC_YEAR}.profile"
 docker image rm "$TAGNAME" 1>&2
